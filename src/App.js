dbf6ade95f941cc6cb45e2e5d9fad2e47e2ecc2c
Kylie Stanley <knstanley11@gmail.com>
2019-02-05 14:23:18 -0700

    Add endpoint sections to HTML

--- src/App.js
+++ src/App.js
@@ -22,6 +22,41 @@ class App extends Component {
              This API allows you to find data on all major 2019 music festivals 
              happening in over 20 states. Data is stored with a PostgresSQL database 
              and the backend is built with Node.js, Express and Knex.js.</p>
+    
+              <h3>States Endpoints</h3>
+
+                <code>GET /api/v1/states</code> 
+                <p>--> returns an array of state objects</p>
+
+                <code>POST /api/v1/states</code> 
+
+                <code>GET /api/v1/states:stateID</code> 
+                <p>--> returns an array with a specific state</p>
+
+                <code>GET  /api/v1/states?state_id=1</code> 
+                <p>--> returns an array with a specific state</p>
+
+                <code>PUT /api/v1/states/:stateID</code>
+
+                <code>DELETE /api/v1/states/:stateID</code>
+
+              <h3>Festivals Endpoints</h3>
+
+                <code>GET /api/v1/festivals</code>
+                <p>--> returns an array of festival objects</p>
+
+                <code>POST /api/v1/festivals</code>
+
+                <code>GET /api/v1/states/:stateID/festivals</code>
+                <p>--> returns an array of festival objects in a  state</p>
+
+                <code>GET /api/v1/festivals/:festivalID</code>
+                <p>--> returns an array with a specific festival </p>
+
+                <code>PUT /api/v1/festivals/:festivalID</code>
+
+                <code>DELETE /api/v1/festivals/:festivalID</code>
+
           </main>
         </div>
       </div>

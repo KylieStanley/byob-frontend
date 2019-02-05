cffec922e3342f66c864da2480f6c2a170ce51df
Kylie Stanley <knstanley11@gmail.com>
2019-02-05 14:14:48 -0700

    Set up basic html structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,24 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-
+          <h1>BYOB FrontEnd</h1>
         </header>
+        <div className="main">
+          <nav>
+            <h4>Table of Contents</h4>
+            <a href="">endpoint</a>
+            <a href="">endpoint</a>
+            <a href="">endpoint</a>
+            <a href="">endpoint</a>
+            <a href="">endpoint</a>
+          </nav>
+          <main>
+            <p>Are you building a tourism, vacation or music events/performances app?
+             This API allows you to find data on all major 2019 music festivals 
+             happening in over 20 states. Data is stored with a PostgresSQL database 
+             and the backend is built with Node.js, Express and Knex.js.</p>
+          </main>
+        </div>
       </div>
     );
   }

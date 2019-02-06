5a7f283050330c54131ba216c3a678c81f9a3d69
Joel Smith <joelwsmith123@gmail.com>
2019-02-05 17:48:53 -0700

    Add toggle state on icon click in Drawer

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Drawer.css'
 import Form from './Form'
 import ExampleContainer from './ExampleContainer'
 
-const Drawer = () => {
-  return (
-  <div className="drawer">
-    <div className="tab-container">
-      <div className="mock-side"></div>
-      <div className="tab">
-        <i class="fas fa-bars"></i>
+class Drawer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showDrawer: false
+    }
+  }
+
+  changeDrawerDisplayState = (event) => {
+    event.preventDefault()
+    this.setState({ showDrawer: !this.state.showDrawer })
+  }
+
+  render() {
+    return (
+      <div className="drawer">
+        <div className="tab-container">
+          <div className="mock-side"></div>
+          <div className="tab">
+            <i onClick={ this.changeDrawerDisplayState } className="fas fa-bars"></i>
+          </div>
+        </div>
+
+        <div>
+          <Form />
+          <ExampleContainer />
+        </div>
       </div>
-    </div>
-
-    <div>
-      <Form />
-      <ExampleContainer />
-    </div>
-
-  </div>
-  )
+    )    
+  }
 }
 
 export default Drawer
